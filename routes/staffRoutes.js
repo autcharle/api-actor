8c199892d4fa61101617719ea47d842ac01f0e21
Tuyen Tran <3t.charle@gmail.com>
2025-03-27 08:26:41 +0700

    fix: update auth route to be post method

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -3,5 +3,5 @@ import { StaffController } from "../controllers/staffController.js";
 
 const router = express.Router();
 const controller = new StaffController();
-router.get("/", controller.getStaffByCredentials);
+router.post("/", controller.getStaffByCredentials);
 export default router;
