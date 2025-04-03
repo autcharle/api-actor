0df2a4c9ba1403d38783b024437a0631c8ecb386
Tuyen Tran <3t.charle@gmail.com>
2025-04-03 21:59:50 +0700

    feature: access token

--- routes/staffRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { StaffController } from "../controllers/staffController.js";
-
-const router = express.Router();
-const controller = new StaffController();
-router.post("/", controller.getCredentialsByStaff);
-export default router;
