026fd76ee2c80b7175f7335acbf8bbc9ba9ffe20
Tuyen Tran <3t.charle@gmail.com>
2025-04-04 10:37:21 +0700

    fix: update secret key for generating refresh token

--- middlewares/generateRefreshToken.js
+++ middlewares/generateRefreshToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const generateRefreshToken = (payload) =>
-  jwt.sign(payload, process.env.CLIENT_SECRET, {
+  jwt.sign(payload, process.env.RF_SECRET, {
     expiresIn: "7d",
   });
